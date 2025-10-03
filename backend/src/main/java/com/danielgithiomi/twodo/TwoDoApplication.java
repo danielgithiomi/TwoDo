5a6b9b66f3204498343a91a1b3f8360387d71932
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-03 10:06:43 +0400

    Setup: Added the backend folder with scaffold springboot project

---
+++ backend/src/main/java/com/danielgithiomi/twodo/TwoDoApplication.java
@@ -0,0 +1,13 @@
+package com.danielgithiomi.twodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoDoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/danielgithiomi/twodo/TwoDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielgithiomi.twodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
