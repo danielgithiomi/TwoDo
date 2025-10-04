56428aab1250eb53f57ac7fa75fdb914a6352d35
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-04 12:03:35 +0400

    Setup (Server-Configuration) 'TD-001': Added application properties binding, updated banner.txt, and enhanced application.yml configuration
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

---
+++ server/src/main/java/com/danielgithiomi/twodo/data/records/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.danielgithiomi.twodo.data.records;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "twodo.application")
+public record ApplicationProperties(
+        String name,
+        String version,
+        String developer,
+        String description,
+        String repository,
+        Boolean manualDBPopulationEnabled
+) {
+}
