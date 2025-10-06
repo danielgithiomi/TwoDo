920938c5e8a824e00c2c3705b922407a1d497801
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-06 19:35:58 +0400

    Setup (Frontend-Configuration) 'TD-001': Updated Vite aliases, tsconfig paths, and build settings to include atom components, enable declaration file generation, and integrate shared packages
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/apps/web/src/App.tsx
+++ frontend/apps/web/src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import appLogo from "@tdw/images/branding/alt_logo.png";
+import { Button } from "@tdw/atoms";
 
 function App() {
-  return <img src={appLogo} alt="Application Logo" />;
+  return (
+    <>
+      <img src={appLogo} alt="Application Logo" />
+      <Button />
+    </>
+  );
 }
 
 export default App;
--- frontend/apps/web/vite.config.ts
+++ frontend/apps/web/vite.config.ts
@@ -1,24 +1,25 @@
-import path from 'path';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      "@tdw/images": path.resolve(__dirname, './public/assets/images'),
-    },
+      "@": path.resolve(__dirname, "./src"),
+      "@tdw/images": path.resolve(__dirname, "./public/assets/images"),
+      "@tdw/atoms": path.resolve(__dirname, "./../../packages/components/atoms"),
+    }
   },
   server: {
     fs: {
-      allow: ['..'],
-    },
+      allow: [".."]
+    }
   },
-  publicDir: 'public',
+  publicDir: "public",
   build: {
-    assetsDir: 'assets',
-    copyPublicDir: true,
-  },
+    assetsDir: "assets",
+    copyPublicDir: true
+  }
 });
