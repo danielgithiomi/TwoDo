1bc4de55ee7dc87859d5ec261f615a9a44008026
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-12 16:20:18 +0400

    Setup (Backend-Configuration) 'TD-001': Updated `application-development.yml` to use default H2 credentials, enabled H2 console, and added a `SecurityFilterChain` bean to configure HTTP security for H2 access and authentication.
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- server/src/main/java/com/danielgithiomi/twodo/config/SecurityConfig.java
+++ server/src/main/java/com/danielgithiomi/twodo/config/SecurityConfig.java
@@ -3,22 +3,43 @@ package com.danielgithiomi.twodo.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .headers(headers -> headers.frameOptions().disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/h2/**").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .build();
+    }
 
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
