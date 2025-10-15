8d48d9287711f25d47113c4985e127dc0492e6b8
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-15 20:15:14 +0400

    Feat (Auth-Service) 'TD-005': Implemented `AuthUserDetailsService` and `AuthUser` for Spring Security integration.
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

---
+++ server/src/main/java/com/danielgithiomi/twodo/security/AuthUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.danielgithiomi.twodo.security;
+
+import com.danielgithiomi.twodo.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // Get the userfrom the DB
+        return null;
+    }
+}
