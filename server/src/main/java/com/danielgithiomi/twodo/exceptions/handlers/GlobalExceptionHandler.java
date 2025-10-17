f8233f4795e4b486b0a789d6754107ad242f977b
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-17 15:27:23 +0400

    Feat (Auth-Service) 'TD-006': Added `IllegalArgumentException` handling in `GlobalExceptionHandler`
    - Introduced dedicated handler for `IllegalArgumentException` to return `BAD_REQUEST` responses.
    - Refactored `UserAlreadyExistsException` handler to use dynamic `CONFLICT` status.
    - Enhanced consistency in HTTP status handling within `GlobalExceptionHandler`.
    
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- server/src/main/java/com/danielgithiomi/twodo/exceptions/handlers/GlobalExceptionHandler.java
+++ server/src/main/java/com/danielgithiomi/twodo/exceptions/handlers/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.danielgithiomi.twodo.exceptions.handlers;
 import com.danielgithiomi.twodo.domains.models.api.ApiErrorResponse;
 import com.danielgithiomi.twodo.exceptions.UserAlreadyExistsException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
 
 @Slf4j
@@ -18,11 +20,24 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<ApiErrorResponse> userAlreadyExistsException(UserAlreadyExistsException ex) {
 
-        log.error("UserAlreadyExistsException: {}", ex.getMessage());
+        HttpStatus status = CONFLICT;
 
-        return ResponseEntity.badRequest().body(
+        return ResponseEntity.status(status).body(
                 ApiErrorResponse.builder()
-                        .statusCode(CONFLICT.value())
+                        .statusCode(status.value())
+                        .message(ex.getMessage())
+                        .build());
+
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> illegalArgumentException(IllegalArgumentException ex) {
+
+        HttpStatus status = BAD_REQUEST;
+
+        return ResponseEntity.status(status).body(
+                ApiErrorResponse.builder()
+                        .statusCode(status.value())
                         .message(ex.getMessage())
                         .build());
 
