44a81fd34959b866a6e2692a944bb5e859b46ff4
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-21 15:55:05 +0400

    Ref (Auth-Service) 'TD-005': Replaced `AuthenticationManager` configuration with `ProviderManager`
    - Introduced `DaoAuthenticationProvider` in `SecurityConfig` for custom user details and password handling.
    - Replaced `AuthenticationConfiguration.getAuthenticationManager()` with `ProviderManager` to streamline authentication setup.
    - Removed direct `userDetailsService` configuration from `HttpSecurity` for cleaner integration.
    
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- server/src/main/java/com/danielgithiomi/twodo/config/SecurityConfig.java
+++ server/src/main/java/com/danielgithiomi/twodo/config/SecurityConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -45,7 +47,6 @@ public class SecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/api/v1/users/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .userDetailsService(userDetailsService)
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults())
                 .build();
@@ -57,8 +58,15 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(authenticationProvider());
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
     @Bean
