ea972077902e605f0d801503ab1ad746d7a98784
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-22 11:44:39 +0400

    Feat (Auth-Service) 'TD-005': Enhanced JWT generation with expiration and signing logic
    - Added issuer, issued date, and expiration date to JWTs in `AuthServiceImpl`.
    - Introduced signing logic using `SecretKey` derived from Base64-encoded `JWT_SECRET`.
    - Refactored duration calculation with new utility method `hrsToMillis` in `HelperFunctions`.
    
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- server/src/main/java/com/danielgithiomi/twodo/security/AuthServiceImpl.java
+++ server/src/main/java/com/danielgithiomi/twodo/security/AuthServiceImpl.java
@@ -1,18 +1,30 @@
 package com.danielgithiomi.twodo.security;
 
 import com.danielgithiomi.twodo.security.interfaces.AuthService;
+import com.danielgithiomi.twodo.utils.HelperFunctions;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.Base64;
 import java.util.Map;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
+    @Value("${twodo.application.name}")
+    private static String JWT_ISSUER;
+
     @Value("${twodo.application.jwtConfig.JWTSecret}")
     private static String JWT_SECRET;
+
+    @Value("${twodo.application.jwtConfig.JWTValidityDurationInHrs}")
+    private static int JWT_VALIDITY_DURATION_IN_HRS;
 
     @Override
     public UserDetails validateUser(String username, String password) {
@@ -21,9 +33,15 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public String generateJwtToken(UserDetails userDetails) {
+        Long jwtDuration = HelperFunctions.hrsToMillis(JWT_VALIDITY_DURATION_IN_HRS);
+
         return Jwts.builder()
+                .issuer(JWT_ISSUER)
+                .issuedAt(Date.from(Instant.now()))
                 .subject(userDetails.getUsername())
                 .claims(createJwtClaims(userDetails))
+                .expiration(Date.from(Instant.now().plusMillis(jwtDuration)))
+                .signWith(generateSignWithKey())
                 .compact();
     }
 
@@ -35,5 +53,10 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public Map<String, Object> createJwtClaims(UserDetails userDetails) {
         return Map.of();
+    }
+
+    private SecretKey generateSignWithKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(JWT_SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
