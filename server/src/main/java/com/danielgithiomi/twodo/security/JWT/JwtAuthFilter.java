086542f8fcfd10539b09d16b4393f1e0343aef2d
Daniel Githiomi <danielgithiomi@gmail.com>
2025-10-23 11:16:23 +0400

    Ref (Auth-Service) 'TD-005': Added `@NonNull` annotations in `JwtAuthFilter` method parameters for null-safety enforcement
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- server/src/main/java/com/danielgithiomi/twodo/security/JWT/JwtAuthFilter.java
+++ server/src/main/java/com/danielgithiomi/twodo/security/JWT/JwtAuthFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,9 +30,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         String jwtToken = extractJwtToken(request);
         if (jwtToken == null) {
