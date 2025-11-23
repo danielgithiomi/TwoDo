00c4f74161b04f09d98754884fc5bc80782986e1
Daniel Githiomi <danielgithiomi@gmail.com>
2025-11-23 17:28:18 +0400

    Feat (Frontend) 'TD-007-gender-service': Integrated `useGender` hook and gender data rendering in App component
    - Imported and utilized `useGender` hook from `@tdp/api` in `App.tsx`.
    - Rendered gender list dynamically using retrieved gender data.
    
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/apps/web/src/App.tsx
+++ frontend/apps/web/src/App.tsx
@@ -1,17 +1,25 @@
 import "./App.css";
 import { Button } from "@tdw/atoms";
+import { useGender } from "@tdp/api";
 import { DummyData } from "./DummyData";
 import alt_logo from "@tdw/images/branding/alt_logo.png";
 
 function App() {
-
-  
+  const { genders } = useGender();
 
   return (
     <>
       <img src={alt_logo} alt="Application Logo" />
       <Button />
       <DummyData />
+
+      <ul>
+        {genders.map((gender) => (
+          <li className="text-white" key={gender}>
+            {gender}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
