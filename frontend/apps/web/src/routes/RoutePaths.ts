a7d1c57f297025b205d8bdc7558765ff1d4afc9f
Daniel Githiomi <danielgithiomi@gmail.com>
2025-11-23 19:33:20 +0400

    Feat (Frontend) 'TD-001-project-setup-and-configuration': Setup and tested application styling and routing

--- frontend/apps/web/src/routes/RoutePaths.ts
+++ frontend/apps/web/src/routes/RoutePaths.ts
@@ -3,5 +3,6 @@ import type { RouteDestination } from "./Routes.types";
 export const RoutePaths: Record<RouteDestination, string> = {
   Home: "/",
   Login: "/login",
+  SignUp: "/sign-up",
   NotFound: "*",
 } as const;
