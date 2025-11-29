843638358ba8397c60b99918cd722b030d239cdd
Daniel Githiomi <danielgithiomi@gmail.com>
2025-11-29 17:40:52 +0400

    Feat (Frontend) 'TD-008': Added utility function `omit` for omitting keys from objects in `utils.ts`
    Signed-off-by: Daniel Githiomi <danielgithiomi@gmail.com>

--- frontend/packages/libs/utils.ts
+++ frontend/packages/libs/utils.ts
@@ -12,3 +12,23 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: Parameters<typeof classNames>) {
   return twMerge(classNames(...inputs));
 }
+
+/* -------------------------------------------------------------------------------------------------
+ * Omit
+ * -----------------------------------------------------------------------------------------------*/
+/**
+ * function to omit keys from an object
+ * @param obj - The object to omit keys from
+ * @param keys - The keys to omit
+ * @returns The object with the specified keys omitted
+ */
+export function omit<T extends Record<string, any>, K extends keyof T>(
+  obj: T,
+  keys: K[]
+): Omit<T, K> {
+  const clone = { ...obj };
+  for (const key of keys) {
+    delete clone[key];
+  }
+  return clone;
+}
